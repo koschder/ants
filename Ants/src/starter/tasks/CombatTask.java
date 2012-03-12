7b6215462075852ff9fbbdfb6135e7d182538bbb
Lukas Kuster <koschder@gmail.com>
2012-03-12 20:17:55 +0100

    additional tasks

---
+++ Ants/src/starter/tasks/CombatTask.java
@@ -0,0 +1,16 @@
+package starter.tasks;
+
+import java.util.Map;
+
+import starter.Ants;
+import starter.Tile;
+
+public class CombatTask extends BaseTask {
+
+	@Override
+	public void perform(Ants ants, Map<Tile, Tile> orders) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
