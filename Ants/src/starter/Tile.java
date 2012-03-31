f6c3dc0ac34be204a4c8787c4d24bf6f6c5ccb4a
Stefan Käser <info.stef.kaeser@gmail.com>
2012-03-31 20:30:46 +0200

    formatting only

--- Ants/src/starter/Tile.java
+++ Ants/src/starter/Tile.java
@@ -81,20 +81,20 @@ public class Tile implements Comparable<Tile> {
     }
 
     public Aim directionTo(Tile nextStep) {
-        if(getRow() == nextStep.getRow()){
+        if (getRow() == nextStep.getRow()) {
             // changing in column east or west
             int diff = getCol() - nextStep.getCol();
-            if(diff == getCol() || (diff < 0 && diff != -getCol())){
+            if (diff == getCol() || (diff < 0 && diff != -getCol())) {
                 return Aim.EAST;
-            }else{
+            } else {
                 return Aim.WEST;
             }
-        }else{
+        } else {
             // changing in row north or south
             int diff = getRow() - nextStep.getRow();
-            if(diff == getRow() || (diff < 0 && diff != -getRow())){
+            if (diff == getRow() || (diff < 0 && diff != -getRow())) {
                 return Aim.SOUTH;
-            }else{
+            } else {
                 return Aim.NORTH;
             }
         }
