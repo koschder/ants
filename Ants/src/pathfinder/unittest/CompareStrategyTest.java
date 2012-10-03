72ded3399292cb9a6bd0f2f717e4d759027d728e
SteffStefferson <info.stef.kaeser@gmail.com>
2012-10-03 19:32:39 +0200

    CompareStrategyTest.java

---
+++ Ants/src/pathfinder/unittest/CompareStrategyTest.java
@@ -0,0 +1,71 @@
+package pathfinder.unittest;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import pathfinder.PathFinder;
+import pathfinder.entities.Tile;
+
+public class CompareStrategyTest {
+
+    @Test
+    public void AStarTest() {
+        String sTestName = "AStarTest";
+        System.out.println(sTestName);       
+        PathFinder pf = initPathFinder();
+        Tile start =  new Tile(25, 70);
+        Tile end = new Tile(45, 20);
+        List<Tile> path = pf.search(PathFinder.Strategy.AStar,start, end, -1);
+        if(path == null){
+            path = new ArrayList<Tile>();
+            path.add(start);
+            path.add(end);
+        }
+            
+        ((UnitTestMap)pf.getMap()).saveHtmlMap(sTestName, path,-1);
+        Assert.assertNotNull(path);
+
+    }
+
+    private PathFinder initPathFinder() {
+        UnitTestMap map = initMap();
+        PathFinder pf = new PathFinder();
+        pf.setMap(map);       
+        return pf;
+    }
+
+    private UnitTestMap initMap() {
+        String sFileName = "tools\\maps\\cell_maze\\cell_maze_p04_10.map";
+        try {
+            UnitTestMap map = new UnitTestMap(parseFile(sFileName));
+            return map;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    private static List<String> parseFile(String filePath) throws Exception {
+        List<String> rows = new ArrayList<String>();
+
+        FileReader fr = new FileReader(filePath);
+        BufferedReader br = new BufferedReader(fr);
+
+        String currentRecord;
+        while ((currentRecord = br.readLine()) != null) {
+            if (currentRecord.startsWith("m "))
+                rows.add(currentRecord.substring(2));
+        }
+
+        br.close();
+
+        return rows;
+    }
+}
