5d93ce390697783af2b936d49f2d7270818da501
SteffStefferson <info.stef.kaeser@gmail.com>
2012-11-07 18:57:45 +0100

    ConcentrateMission.java approved

--- Ants/src/ants/missions/ConcentrateMission.java
+++ Ants/src/ants/missions/ConcentrateMission.java
@@ -1,6 +1,7 @@
 package ants.missions;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import logging.Logger;
@@ -19,7 +20,17 @@ public class ConcentrateMission implements Mission {
 
     public List<Ant> ants = new ArrayList<Ant>();
     public Tile troopPoint;
+    public Tile offsetPoint;
     public int amount;
+    private float diffx = 0;
+    private float diffy = 0;
+
+    public enum ContcentrateType {
+        Radial,
+        Square,
+        Line
+    };
+
     /**
      * How far an ant can be to get involved into the mission
      */
@@ -36,6 +47,7 @@ public class ConcentrateMission implements Mission {
     public ConcentrateMission(Tile tP, int a) {
         this.troopPoint = tP;
         this.amount = a;
+
         LOGGER.info("TroopMission_created TroopPoint %s Ants needed %S", troopPoint, amount);
     }
 
@@ -46,7 +58,7 @@ public class ConcentrateMission implements Mission {
 
         for (Ant a : ants) {
             // TODO define max manhattanDistance considering amount
-            int maxDistance = 1 + (amount / 4); // vierer nachbarschaft.
+            int maxDistance = (amount / 4); // vierer nachbarschaft.
             if (Ants.getWorld().manhattanDistance(a.getTile(), troopPoint) > maxDistance) {
                 LOGGER.info("Ant %s it to far away of TroopPoint %s. mission insn't compelete yet", a, troopPoint);
                 return false;
@@ -65,22 +77,90 @@ public class ConcentrateMission implements Mission {
     @Override
     public void execute() {
         LOGGER.info("TroopMission_Executed TroopPoint %s Ants:  %s", troopPoint, ants.toString());
+        calculateBalancePoint();
         for (Ant a : ants) {
             moveAnt(a);
         }
     }
 
+    private void calculateBalancePoint() {
+        diffx = 0;
+        diffy = 0;
+        for (Ant a : ants) {
+            // diffx += Math.abs(a.getTile().getCol() - troopPoint.getCol());
+            diffx += a.getTile().getCol();
+            // diffy += Math.abs(a.getTile().getRow() - troopPoint.getRow());
+            diffy += a.getTile().getRow();
+        }
+
+        int troopPointFactor = ants.size();
+
+        diffx += troopPoint.getCol() * troopPointFactor;
+        diffy += troopPoint.getRow() * troopPointFactor;
+
+        diffx = diffx / (ants.size() + troopPointFactor);
+        diffy = diffy / (ants.size() + troopPointFactor);
+
+        offsetPoint = new Tile((int) diffy, (int) diffx);
+        LOGGER.info("TroopMission_calculateBalancePoint x: %s y: %s offsetPoint %s", diffx, diffy, offsetPoint);
+    }
+
+    private void moveAntOld(Ant a) {
+        boolean bIssueOrderd = false;
+        if (!bIssueOrderd && !a.getTile().equals(troopPoint)) {
+            // move towards TroopPoint
+            List<Aim> aims = Ants.getWorld().getDirections(a.getTile(), offsetPoint);
+            Collections.shuffle(aims);
+            for (Aim ai : aims) {
+                if (Ants.getOrders().issueOrder(a, ai, "TroopMission")) {
+                    LOGGER.info("TroopMission_AntMoved %s moved to %s", a, ai);
+                    bIssueOrderd = true;
+                    break;
+                }
+            }
+        }
+        if (!bIssueOrderd) {
+            // if no issue could be ordered we do a null move, so that the ant doesn't apear at the
+            // unemployeed list
+            Ants.getOrders().issueOrder(a, null, "TroopMission");
+        }
+
+    }
+
     private void moveAnt(Ant a) {
         boolean bIssueOrderd = false;
-        if (false) {
-            // here we do the positioning
-
-        } else {
+        int maxDistance = (amount / 4); // vierer nachbarschaft.
+        if (Ants.getWorld().manhattanDistance(a.getTile(), troopPoint) < maxDistance) {
+            Aim aim = null;
+            if (diffx < diffy) {
+                boolean north = (diffx < 0);
+                aim = north ? Aim.NORTH : Aim.SOUTH;
+            } else {
+                boolean east = (diffy < 0);
+                aim = east ? Aim.EAST : Aim.WEST;
+            }
+            if (Ants.getOrders().issueOrder(a, aim, "TroopMission")) {
+                LOGGER.info("TroopMission_AntMoved %s moved to %s", a, aim);
+                bIssueOrderd = true;
+            }
+        }
+        if (!bIssueOrderd && !a.getTile().equals(troopPoint)) {
             // move towards TroopPoint
             List<Aim> aims = Ants.getWorld().getDirections(a.getTile(), troopPoint);
-            for (Aim aim : aims) {
-                if (Ants.getOrders().issueOrder(a, aim, "TroopMission")) {
-                    LOGGER.info("TroopMission_AntMoved %s moved to %s", a, aim);
+            Collections.shuffle(aims);
+            for (Aim ai : aims) {
+                if (Ants.getOrders().issueOrder(a, ai, "TroopMission")) {
+                    LOGGER.info("TroopMission_AntMoved %s moved to %s", a, ai);
+                    bIssueOrderd = true;
+                    break;
+                }
+            }
+        } else {
+            Aim[] aims = { Aim.NORTH, Aim.WEST, Aim.SOUTH, Aim.EAST };
+            // Collections.shuffle(aims);
+            for (Aim ai : aims) {
+                if (Ants.getOrders().issueOrder(a, ai, "TroopMission")) {
+                    LOGGER.info("TroopMission_AntMoved %s moved to %s", a, ai);
                     bIssueOrderd = true;
                     break;
                 }
