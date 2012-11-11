ba915557dcc33c173690308f854b967bfc643e0b
SteffStefferson <info.stef.kaeser@gmail.com>
2012-11-11 21:42:57 +0100

    HotCommit: Approved AdditionalInformation.js, display the paths on the canvas.

--- Ants/tools/visualizer/js/AdditionalInformation.js
+++ Ants/tools/visualizer/js/AdditionalInformation.js
@@ -5,7 +5,31 @@ function displayAdditionalInformation(vis){
     var pos = (parseInt(vis.antsMap.turn)+1)+'#'+vis.antsMap.mouseRow+'#'+vis.antsMap.mouseCol;
     //var title = (parseInt(vis.antsMap.turn)+1)+'#'+vis.antsMap.mouseRow+'#'+vis.antsMap.mouseCol;
     if(informations[pos] != null){
-        ShowInfoPopup('Info for: '+pos,informations[pos]);
+		
+		var val = informations[pos];
+	
+		var linkedval = "";
+		
+		while(val.indexOf('r:') >= 0 && val.indexOf('c:') >= 0){
+			//<r:20 c:39>
+			linkedval += val.substr(0,val.indexOf(';r:')+1);
+			val = val.substr(val.indexOf(';r:')+1);
+			
+			var text = val.substr(0,val.indexOf('>'));
+			
+			val = val.substr(text.length);
+			
+			var row = text.substr(2,text.indexOf(' ')-2);
+			var col = text.substr(text.indexOf('c')+2);
+			showTile(row,col);			
+			linkedval += "<a href=\"javascript:showTile("+row+","+col+");\">"+text+"</a>";
+		}
+		
+		
+		
+		linkedval += val;
+		
+        ShowInfoPopup('Info for: '+pos,linkedval);
         
     }
 }
@@ -19,13 +43,27 @@ function ShowInfoPopup(title, text) {
     })
 };
 
+function showTile(row,col){
+
+//this.hint row 40 | col 39
+//var hintPos = "row "+row+" | col "+col; // 
+//alert();
+//alert(hintPos);
+//visualizer.hint = hintPos;
+var scale = visualizer.map.scale;
+visualizer.map.ctx.fillStyle = "#ee9933";
+visualizer.map.ctx.fillRect( col*scale,row*scale, scale, scale);
+//visualizer.director.draw();
+}
+
 
 $.getJSON($liveInfoFile, function(data) {
     var i = 0;
   $.each(data, function(key, val) {
     
 	if(key.indexOf("#") > 0){
-	key = key.substr(key.indexOf("#")+1);
+		key = key.substr(key.indexOf("#")+1);
+		
 		if(informations[key])
 		{
 			informations[key] = informations[key]+"<br/><br/>"+val;
