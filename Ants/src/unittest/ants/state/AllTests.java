4855fb9a6bc0165e585bb0af94bbe4fb54926f6c
Lukas Kuster <koschder@gmail.com>
2012-11-12 18:14:08 +0100

    Add Test Suite to run all tests at once

---
+++ Ants/src/unittest/ants/state/AllTests.java
@@ -0,0 +1,10 @@
+package unittest.ants.state;
+
+import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.extensions.cpsuite.ClasspathSuite.ClassnameFilters;
+import org.junit.runner.RunWith;
+
+@RunWith(ClasspathSuite.class)
+@ClassnameFilters(".*Test")
+public class AllTests {
+}
