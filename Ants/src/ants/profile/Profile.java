e1e2f84f21599f33c0351962d3dbe23ae4024b59
Lukas Kuster <koschder@gmail.com>
2012-12-28 12:18:28 +0100

    parameterized PercentExploredRule

--- Ants/src/ants/profile/Profile.java
+++ Ants/src/ants/profile/Profile.java
@@ -18,12 +18,39 @@ public class Profile {
     private static final Logger LOGGER = LoggerFactory.getLogger(LogCategory.SETUP);
 
     private enum Key {
+        /**
+         * Default allocation of resources for gathering food.
+         */
         DEFAULT_ALLOCATION_GATHER_FOOD("defaultAllocation.gatherFood"),
+        /**
+         * Default allocation of resources for exploration.
+         */
         DEFAULT_ALLOCATION_EXPLORE("defaultAllocation.explore"),
+        /**
+         * Default allocation of resources for attacking hills.
+         */
         DEFAULT_ALLOCATION_ATTACK_HILLS("defaultAllocation.attackHills"),
+        /**
+         * Default allocation of resources for defending hills.
+         */
         DEFAULT_ALLOCATION_DEFEND_HILLS("defaultAllocation.defendHills"),
+        /**
+         * Turn at which we start defending hills even if no enemy is nearby.
+         */
         DEFEND_HILLS_START_TURN("defendHills.startTurn"),
-        DEFEND_HILLS_MIN_CONTROL_RADIUS("defendHills.minControlRadius");
+        /**
+         * Minimum control radius for hill defense (measured in tiles).
+         */
+        DEFEND_HILLS_MIN_CONTROL_RADIUS("defendHills.minControlRadius"),
+        /**
+         * Threshold at which exploration is granted additional resources, expressed in percent of invisible tiles.
+         */
+        EXPLORE_FORCE_THRESHOLD_PERCENT("explore.forceThresholdPercent"),
+        /**
+         * Determines how much exploration is boosted when the threshold is met. Expressed as a fraction of 1, default
+         * is 0.25.
+         */
+        EXPLORE_FORCE_GAIN("explore.forceGain");
         private String key;
 
         private Key(String key) {
@@ -41,6 +68,8 @@ public class Profile {
         defaultProperties.setProperty(Key.DEFAULT_ALLOCATION_DEFEND_HILLS.key, "25");
         defaultProperties.setProperty(Key.DEFEND_HILLS_START_TURN.key, "30");
         defaultProperties.setProperty(Key.DEFEND_HILLS_MIN_CONTROL_RADIUS.key, "8");
+        defaultProperties.setProperty(Key.EXPLORE_FORCE_THRESHOLD_PERCENT.key, "80");
+        defaultProperties.setProperty(Key.EXPLORE_FORCE_GAIN.key, "0.25");
     }
 
     /*
@@ -71,6 +100,14 @@ public class Profile {
         return getInteger(Key.DEFEND_HILLS_MIN_CONTROL_RADIUS);
     }
 
+    public int getExplore_ForceThresholdPercent() {
+        return getInteger(Key.EXPLORE_FORCE_THRESHOLD_PERCENT);
+    }
+
+    public float getExplore_ForceGain() {
+        return getFloat(Key.EXPLORE_FORCE_GAIN);
+    }
+
     public Profile(String profile) {
         properties = new Properties(defaultProperties);
         if (profile != null) {
@@ -97,11 +134,18 @@ public class Profile {
                 + getDefaultAllocation_Explore() + getDefaultAllocation_GatherFood();
         if (totalDefaultAllocation != 100)
             throw new InvalidProfileConfigurationException("Default resource allocations do not add up to 100");
-
+        if (getExplore_ForceThresholdPercent() > 100 || getExplore_ForceThresholdPercent() < 0)
+            throw new InvalidProfileConfigurationException("explore.forceThresholdPercent must be between 0 and 100");
+        if (getExplore_ForceGain() > 1.0f || getExplore_ForceGain() < 0.0f)
+            throw new InvalidProfileConfigurationException("explore.forceGain must be between 0.0 and 1.0");
     }
 
     private int getInteger(Key key) {
         return Integer.valueOf(properties.getProperty(key.key));
+    }
+
+    private float getFloat(Key key) {
+        return Float.valueOf(properties.getProperty(key.key));
     }
 
     private String getPropertiesAsString() {
--- Ants/src/ants/strategy/rules/PercentExploredRule.java
+++ Ants/src/ants/strategy/rules/PercentExploredRule.java
@@ -11,8 +11,8 @@ public class PercentExploredRule extends BaseResourceAllocationRule {
      */
     public void allocateResources() {
         final int visibleTilesPercent = Ants.getWorld().getVisibleTilesPercent();
-        if (visibleTilesPercent < 80) {
-            int increment = Math.round((100 - visibleTilesPercent) / 4);
+        if (visibleTilesPercent < Ants.getProfile().getExplore_ForceThresholdPercent()) {
+            int increment = Math.round((100 - visibleTilesPercent) * Ants.getProfile().getExplore_ForceGain());
             Type[] tasksToDecrement = new Type[] { Type.ATTACK_HILLS, Type.GATHER_FOOD, Type.DEFEND_HILL };
             increment -= increment % tasksToDecrement.length;
             incrementResources(Type.EXPLORE, increment, tasksToDecrement);
