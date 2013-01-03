73228f98f1bdf6a5e217377f248dfb742388eeac
Lukas Kuster <koschder@gmail.com>
2013-01-03 14:10:09 +0100

    renamed influence test package for consistency

---
+++ Strategy/src/unittest/influence/InfluenceTest.java
@@ -0,0 +1,127 @@
+package unittest.influence;
+
+import influence.DefaultInfluenceMap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import api.entities.Tile;
+import api.strategy.InfluenceMap;
+import api.test.MapOutput;
+import api.test.PixelDecorator;
+
+import static org.junit.Assert.*;
+
+/**
+ * the class containing all unit tests to prove the functionalities of the influence map
+ * 
+ * @author kases1, kustl1
+ * 
+ */
+public class InfluenceTest {
+
+    /**
+     * this test creates an influence map, and writes an html file to see the results
+     */
+    @Test
+    public void safetyInfluenceTest() {
+
+        System.out.println("safetyInfluenceTest");
+        String sMap = getDefaultTestMap();
+
+        InfluenceTestMap map = new InfluenceTestMap(37, sMap);
+
+        final InfluenceMap iMap = new DefaultInfluenceMap(map, 8, 4);
+
+        MapOutput put = new MapOutput();
+        put.setMap(map);
+        put.setClusterSize(5);
+        put.addAllUnits();
+        put.saveHtmlMap("InfluenceTest_safetyInfluenceTest", getSafetyDecorator(iMap));
+
+        Assert.assertEquals(iMap.getInfluence(1, new Tile(2, 4)), iMap.getInfluence(0, new Tile(2, 4)));
+        Assert.assertTrue(iMap.getSafety(new Tile(2, 3)) > 0);
+        Assert.assertTrue(iMap.getSafety(new Tile(2, 5)) < 0);
+
+    }
+
+    private String getDefaultTestMap() {
+        String sMap = "";
+        sMap += "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww";
+        sMap += "woooooooooowwwwwwwwwwooooooooooooooow";
+        sMap += "woo0o1ooooowwwwwwwwwwoo2ooooooo0oooow";
+        sMap += "woooooooooowwwwwwwwwwooooowooooooooow";
+        sMap += "woooooooooowwwwwwwwwwooooowooooooooow";
+        sMap += "wooo0ooowwwwwwwwwwwwwooooowooo1ooooow";
+        sMap += "woooooooooooowoooooooooooowooooooooow";
+        sMap += "wooooooooooo0woooowoooooo2wooooooooow";
+        sMap += "woooo11ooooooooooowooooooowooooooooow";
+        sMap += "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww";
+        return sMap;
+    }
+
+    /**
+     * tests the update mechanism
+     */
+    @Test
+    public void safetyInfluenceUpdateTest() {
+
+        System.out.println("safetyInfluenceTest");
+        String sMap = getDefaultTestMap();
+
+        InfluenceTestMap initialMap = new InfluenceTestMap(37, sMap);
+
+        sMap = "";
+        sMap += "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww";
+        sMap += "woooooooooowwwwwwwwwwooooooooooooooow";
+        sMap += "wo0ooo1oooowwwwwwwwwwooo2ooooo0ooooow";
+        sMap += "woooooooooowwwwwwwwwwooooowooooooooow";
+        sMap += "woooooooooowwwwwwwwwwooooowooooooooow";
+        sMap += "wooooooowwwwwwwwwwwwwooooowooooooooow";
+        sMap += "wooo0oooooooowooooooooooo2wooo1ooooow";
+        sMap += "woooo1ooooo0owoooowooooooowooooooooow";
+        sMap += "wooooooooooooooooowooooooowooooooooow";
+        sMap += "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww";
+
+        InfluenceTestMap updateMap = new InfluenceTestMap(37, sMap);
+
+        final DefaultInfluenceMap iMap = new DefaultInfluenceMap(initialMap, 8, 4);
+        iMap.update(updateMap);
+
+        MapOutput put = new MapOutput();
+        put.setMap(updateMap);
+        put.setClusterSize(5);
+        put.addAllUnits();
+        put.saveHtmlMap("InfluenceTest_safetyInfluenceUpdateTest", getSafetyDecorator(iMap));
+
+    }
+
+    private PixelDecorator getSafetyDecorator(final InfluenceMap iMap) {
+        return new PixelDecorator() {
+
+            @Override
+            public String getLabel(Tile tile) {
+                return String.valueOf(iMap.getSafety(tile));
+            }
+        };
+    }
+
+    /**
+     * tests the total influence function each player
+     */
+    @Test
+    public void totalInfluenceTest() {
+
+        System.out.println("safetyInfluenceTest");
+        String sMap = getDefaultTestMap();
+
+        InfluenceTestMap map = new InfluenceTestMap(37, sMap);
+
+        InfluenceMap iMap = new DefaultInfluenceMap(map, 8, 4);
+
+        assertEquals(2520, iMap.getTotalInfluence(0));
+        assertEquals(2370, iMap.getTotalInfluence(1));
+        assertEquals(1120, iMap.getTotalInfluence(2));
+
+    }
+}
---
+++ Strategy/src/unittest/influence/InfluenceTestMap.java
@@ -0,0 +1,141 @@
+package unittest.influence;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import api.entities.Aim;
+import api.entities.Tile;
+import api.entities.Unit;
+import api.map.AbstractWraparoundMap;
+import api.search.PathPiece;
+import api.strategy.InfluenceMap;
+import api.strategy.SearchableUnitMap;
+import api.test.TestUnit;
+
+/**
+ * this implementation of the AbstractWraparoundMap is only use by the unit tests.
+ * 
+ * @author kases1, kustl1
+ * @see InfluenceTest
+ */
+public class InfluenceTestMap extends AbstractWraparoundMap implements SearchableUnitMap {
+
+    int[][] map;
+    Map<Integer, List<Unit>> players = new HashMap<Integer, List<Unit>>();
+    int water = 0;
+    int land = 1;
+
+    public InfluenceTestMap(int x, String sMap) {
+        super(x, x);
+        List<String> sMapRows = new ArrayList<String>();
+
+        while (sMap.length() >= x) {
+            sMapRows.add(sMap.substring(0, x));
+            sMap = sMap.substring(x);
+        }
+        init(sMapRows);
+    }
+
+    public InfluenceTestMap(List<String> sMapRows) {
+        super(sMapRows.size(), sMapRows.size());
+        init(sMapRows);
+    }
+
+    private void init(List<String> sMapRows) {
+        int mapCols = sMapRows.get(0).length();
+
+        map = new int[sMapRows.size()][mapCols];
+
+        for (int i = 0; i < sMapRows.size(); i++) {
+            for (int j = 0; j < mapCols; j++) {
+                final int row = i;
+                final int col = j;
+                Character charIt = sMapRows.get(i).charAt(j);
+                map[i][j] = charIt == 'w' || charIt == '%' ? water : land;
+
+                if (Character.isDigit(charIt)) {
+                    final int p = Character.getNumericValue(charIt);
+
+                    if (!players.containsKey(p))
+                        players.put(p, new ArrayList<Unit>());
+
+                    players.get(p).add(new TestUnit(p, new Tile(row, col)));
+
+                }
+
+            }
+        }
+    }
+
+    public InfluenceTestMap(int x, int y) {
+        super(x, y);
+        map = new int[x][y];
+        for (int i = 0; i < x; i++)
+            for (int j = 0; j < y; j++)
+                map[i][j] = land;
+    }
+
+    public int getRows() {
+        return map.length;
+    }
+
+    public int getCols() {
+        return map[0].length;
+    }
+
+    public boolean isPassable(Tile tile) {
+        return map[tile.getTargetTile().getRow()][tile.getTargetTile().getCol()] == land;
+    }
+
+    public boolean isVisible(Tile tile) {
+        return true;
+    }
+
+    public List<PathPiece> getSuccessorsForPathfinding(PathPiece target, boolean isNextMove) {
+        Tile state = target.getTargetTile();
+        List<PathPiece> list = new ArrayList<PathPiece>();
+        if (isPassable(getTile(state, Aim.NORTH)))
+            list.add(getTile(state, Aim.NORTH));
+        if (isPassable(getTile(state, Aim.SOUTH)))
+            list.add(getTile(state, Aim.SOUTH));
+        if (isPassable(getTile(state, Aim.WEST)))
+            list.add(getTile(state, Aim.WEST));
+        if (isPassable(getTile(state, Aim.EAST)))
+            list.add(getTile(state, Aim.EAST));
+
+        return list;
+    }
+
+    @Override
+    public List<PathPiece> getSuccessorsForSearch(PathPiece currentEdge, boolean isNextMove) {
+        return getSuccessorsForPathfinding(currentEdge, isNextMove);
+    }
+
+    @Override
+    public List<Unit> getUnits(int player) {
+        final List<Unit> units = players.get(player);
+        return units == null ? new ArrayList<Unit>() : units;
+    }
+
+    @Override
+    public Set<Integer> getPlayers() {
+        return players.keySet();
+    }
+
+    @Override
+    public Tile getSafestNeighbour(Tile tile, InfluenceMap influenceMap) {
+        Tile safestTile = null;
+        int bestSafety = Integer.MIN_VALUE;
+        for (Tile neighbour : get4Neighbours(tile)) {
+            final int safety = influenceMap.getSafety(neighbour);
+            if (safety > bestSafety) {
+                safestTile = neighbour;
+                bestSafety = safety;
+            }
+        }
+        return safestTile;
+    }
+}
