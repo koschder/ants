d58e902bc3fafad6636421c875bb5bb0d191d9fc
Lukas Kuster <koschder@gmail.com>
2013-01-03 15:28:01 +0100

    eliminated useless base class for ResourceAllocator

--- Ants/src/ants/strategy/ResourceAllocator.java
+++ Ants/src/ants/strategy/ResourceAllocator.java
@@ -1,14 +1,27 @@
 package ants.strategy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import logging.Logger;
+import logging.LoggerFactory;
+import ants.LogCategory;
+import ants.missions.Mission;
 import ants.state.Ants;
 import ants.strategy.rules.HalfTimeReachedRule;
 import ants.strategy.rules.PercentExploredRule;
 import ants.strategy.rules.PopulationSizeRule;
 import ants.strategy.rules.RelativeInfluenceRule;
+import ants.strategy.rules.ResourceAllocationRule;
 import ants.tasks.Task.Type;
 import api.strategy.InfluenceMap;
 
-public class ResourceAllocator extends BaseResourceAllocator {
+public class ResourceAllocator {
+    Logger LOGGER = LoggerFactory.getLogger(LogCategory.RESOURCE_ALLOCATION);
+    protected List<ResourceAllocationRule> rules = new ArrayList<ResourceAllocationRule>();
+
     public ResourceAllocator(InfluenceMap influence) {
         // init for all task types
         for (Type type : Type.values()) {
@@ -25,5 +38,32 @@ public class ResourceAllocator extends BaseResourceAllocator {
         rules.add(new RelativeInfluenceRule(influence));
         rules.add(new PercentExploredRule());
         rules.add(new HalfTimeReachedRule());
+    }
+    
+    public void allocateResources() {
+        Map<Type, Integer> antsOnMission = new HashMap<Type, Integer>();
+        for (Mission mission : Ants.getOrders().getMissions()) {
+            Integer ants = antsOnMission.get(mission.getTaskType());
+            if (ants == null)
+                antsOnMission.put(mission.getTaskType(), mission.getAnts().size());
+            else
+                antsOnMission.put(mission.getTaskType(), ants + mission.getAnts().size());
+        }
+        for (Type type : antsOnMission.keySet()) {
+            LOGGER.debug("%s ants currently on mission of type %s", antsOnMission.get(type), type);
+        }
+        for (ResourceAllocationRule rule : rules) {
+            rule.allocateResources();
+        }
+        for (Type type : Type.values()) {
+            final Integer maxResources = Ants.getPopulation().getMaxResources(type);
+            if (maxResources < Integer.MAX_VALUE) {
+                LOGGER.info("Allocated %s percent of ants to task %s", maxResources, type.name());
+            }
+        }
+    }
+
+    public void setRules(List<ResourceAllocationRule> rules) {
+        this.rules = rules;
     }
 }
